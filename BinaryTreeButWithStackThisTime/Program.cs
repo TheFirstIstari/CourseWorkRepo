b28ca66f4d181648ea176b833fa3d1f85af91fed
Lothal <71759730+TheFirstIstari@users.noreply.github.com>
2023-07-13 13:19:38 +0100

    Add files via upload

---
+++ BinaryTreeButWithStackThisTime/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BinaryTreeButWithStackThisTime
+{
+    public class Node
+    {
+        public int data;
+        public Node left, right;
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    // print inorder tree
+    public class BinaryTree
+    {
+        public Node root;
+        public virtual void inorder()
+        {
+            if (root == null)
+            {
+                return;
+            }
+            Stack<Node> stack = new Stack<Node>();
+            Node current = root;
+
+            while(current != null || stack.Count > 0)
+            {
+                while(current != null)
+                {
+                    stack.Push(current);
+                    current = current.left;
+                }
+                current = stack.Pop();
+                Console.Write((char)(current.data) + " ");
+                current = current.right;
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.root = new Node('C');
+            tree.root.left = new Node('I');
+            tree.root.right = new Node('B');
+            tree.root.left.left = new Node('E');
+            tree.root.left.right = new Node('H');
+            tree.root.right.left = new Node('Y');
+            tree.root.right.right = new Node('Q');
+            tree.inorder();
+        }
+    }
+
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace FebTest
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("enter n:  ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            bool found = false;
+            int countofharshadnumbers = 0;
+            int count = 1;
+            while (found == false)
+            {
+               int Sumofdigits = 0;
+                
+                foreach(char num in Convert.ToString(count))
+                {
+                    Sumofdigits = Sumofdigits + num - 48;
+
+                }
+                if (count % Sumofdigits == 0)
+                    countofharshadnumbers++;
+                if(countofharshadnumbers == Convert.ToInt32(number))
+                {
+                    Console.WriteLine(count);
+                    found = true;
+                }
+                count++;
+            }
+
+        }
+
+
+
+        
+
+
+
+
+
+
+
+        
+
+
+    }
+
+}
