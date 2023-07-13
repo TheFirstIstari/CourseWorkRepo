ffb012942162f9fdeb03f901db02cede69102bf7
Lothal <71759730+TheFirstIstari@users.noreply.github.com>
2023-07-13 13:41:04 +0100

    Add files via upload

---
+++ WOOP/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace WOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(9, 5);
+            Rectangle rectangle = new Rectangle(9, 5);
+            
+            Console.WriteLine(triangle.TrangleArea());
+            Console.WriteLine(rectangle.RecangleArea());
+        }
+        
+    }
+    public abstract class Shape
+    {
+        protected  decimal height;
+        protected decimal width;
+
+        public decimal Width { get => width; set => width = value; }
+        public decimal Height { get => height; set => height = value; }
+        public Shape(decimal height, decimal width)
+        {
+            this.height = height;
+            this.width = width; 
+        }
+
+    }
+    class Triangle : Shape
+    {
+        public Triangle(decimal height, decimal width) : base(height, width)
+        {
+            
+        }
+
+        public decimal TrangleArea() {
+            return 0.5M*Width*Height;
+        }
+
+    }
+    class Rectangle : Shape
+    {
+        public Rectangle(decimal height, decimal width) : base(height, width)
+        {
+            
+        }
+        public decimal RecangleArea()
+        {
+            return Width * Height;
+        }
+    }
+}
