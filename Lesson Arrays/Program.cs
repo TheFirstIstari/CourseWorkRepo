eb8f23d8ebf6bb0bd92ea29f4c967c7ee0463f38
Lothal <71759730+TheFirstIstari@users.noreply.github.com>
2023-07-13 13:29:09 +0100

    Add files via upload

---
+++ Lesson Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lesson_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] FootballClub = new string[5];
+                        
+            foreach (string name in FootballClub)
+            {
+                
+                Console.WriteLine(name);
+            }
+            foreach (string name in FootballClub)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ LinearandBinarysearches/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LinearandBinarysearches;
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+        long[] numbers = new long[100]; 
+        Random random = new Random();
+        int a = 0;
+        while (a < numbers.Length)
+        {
+            numbers[a] = random.Next(1, 100);
+            a++;
+        }
+        Array.Sort(numbers);
+        Console.WriteLine("Please enter the number you want to search for:");
+        string searchnumbers = Console.ReadLine();
+        long i = Array.BinarySearch(numbers, searchnumbers);
+        if (i >= 0)
+            Console.WriteLine("number found at position " + i + "\n");
+        else
+            Console.WriteLine("number not found\n");
+        Console.WriteLine("press any key to close\n");
+        Console.ReadKey();
+
+        }
+
+    }
+
