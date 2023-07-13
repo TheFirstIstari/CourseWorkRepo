84db285ce0add1cae1a2c841008a6d77b6d63e9a
Lothal <71759730+TheFirstIstari@users.noreply.github.com>
2023-07-13 13:38:56 +0100

    Add files via upload

---
+++ Strings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace Strings
+
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            /*try
+            {
+                //something
+                int input;
+                Console.WriteLine("PICK A NUMBER: ");
+                input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("your not an idiot");
+            }
+            catch(Exception Ex)
+            {
+                //go to something here
+                Console.WriteLine(Ex.Message);
+            }
+            Console.ReadLine();*/
+            /*string Line = " ";
+            StreamReader file = new StreamReader("quotes.txt");
+            foreach (char Line in file.ReadLine())
+            {
+                Console.WriteLine(Line);
+            }
+            Console.WriteLine();
+            file.Close();*/
+            StreamWriter File = new StreamWriter("quotes.txt", true);
+            File.WriteLine("\"If you stand for nothing, Burr, what will you fall for?\"");
+            File.WriteLine("immigrants, we get the job done");
+            File.WriteLine("\"I will kill your friends and family to remind you of my love.\"");
+            File.Close();
+        }
+    }
+}
---
+++ Ten Thousand and First Prime Number/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Ten_Thousand_and_First_Prime_Number
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stopwatch timerall = new Stopwatch();
+            int no_of_primes_found = 0;
+            int current_number = 0;
+            List<int> found_primes = new List<int>();
+            bool n_minus_found = false;
+            bool n_plus_found = false;
+
+            found_primes.Add(2);
+            found_primes.Add(3);
+            found_primes.Add(5);
+            found_primes.Add(7);
+            found_primes.Add(11);
+            found_primes.Add(13);
+
+            current_number = 12;
+            no_of_primes_found = 6;
+            timerall.Start();
+            Stopwatch timerloop = new Stopwatch();
+            timerloop.Start();
+            while (no_of_primes_found < 10001)
+            {
+                current_number += 6;
+                n_minus_found = false;
+                n_plus_found = false;
+                foreach (int current_prime in found_primes)
+                {
+                    if (!n_minus_found && (current_number - 1) % current_prime == 0)
+                    {
+                        n_minus_found = true;
+                    }
+                    if (!n_plus_found && (current_number + 1) % current_prime == 0)
+                    {
+                        n_plus_found = true;
+                    }
+                    if (n_minus_found && n_plus_found)
+                    {
+                        break;
+                    }
+                }
+                if (!n_minus_found)
+                {
+                    found_primes.Add(current_number - 1);
+                    no_of_primes_found = no_of_primes_found + 1;
+                }
+                if (!n_plus_found && no_of_primes_found < 10001)
+                {
+                    found_primes.Add(current_number + 1);
+                    no_of_primes_found = no_of_primes_found + 1;
+                }
+            }
+            timerall.Stop();
+            timerloop.Stop();
+            Console.WriteLine(no_of_primes_found);
+            Console.WriteLine(Convert.ToString(found_primes[found_primes.Count - 1]));
+            Console.WriteLine(timerall.ElapsedMilliseconds);
+            Console.WriteLine(timerloop.ElapsedMilliseconds);
+        }
+    }
+}
