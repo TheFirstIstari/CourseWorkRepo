a623314c34a7433114d2f3900bd0c1999197f037
Lothal <71759730+TheFirstIstari@users.noreply.github.com>
2023-07-13 13:13:35 +0100

    Add files via upload

---
+++ ackermanprimes/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ackermanprimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            Int16 n = Int16.Parse(Console.ReadLine());
+
+            if (IsPrime(n))
+            {
+                Console.WriteLine($"{n} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} is not a prime number.");
+            }
+        }
+
+        static bool IsPrime(int n)
+        {
+            int m = Ackerman(n, n);
+
+            if (m % n == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static int Ackerman(int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if (m > 0 && n == 0)
+            {
+                return Ackerman(m - 1, 1);
+            }
+            else if (m > 0 && n > 0)
+            {
+                return Ackerman(m - 1, Ackerman(m, n - 1));
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
